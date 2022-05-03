7bc0d0ec5e58410c9252fcda56cbe982ed083d1c
Nicolas Martinez <52309078+Martinez-Latorraca@users.noreply.github.com>
2022-05-02 22:48:28 -0300

    fondo y patron de cancha, falta el select

--- src/field.jsx
+++ src/field.jsx
@@ -1,29 +1,45 @@
-import './App.css';
-import pitch from './pitch.png';
-
-import { useRef } from 'react';
-
-
-
+import "./App.css";
+import { useEffect, useState } from "react";
+import pitch from "./pitch.png";
+import lines from "./lines.png";
+import circles from "./circles.png";
+import squares from "./squares.png";
 
 const Field = (param) => {
+  const images = [`url(${pitch})`];
+  const [imageParam, setImageParam] = useState();
 
-    const images = [`url(${ pitch })`]
-    console.log(param)
+  useEffect(() => {
+    const getImagePattern = () => {
+      switch (param) {
+        case "Lines":
+          return lines;
+        case "Squares":
+          return squares;
+        case "Circles":
+          return circles;
+        default:
+          return circles;
+      }
+    };
+    setImageParam(getImagePattern());
+  }, [param]);
+  images.push(`url(${imageParam})`);
+  images.join(",");
 
- return (
-            <div                
-                style={ {
-                    backgroundColor: "#588f58",
-                    backgroundImage: images,
-                    backgroundPosition:'center',
-                    backgroundRepeat: 'no-repeat',
-                    width: "100%",
-                    height:"700px", 
-                } }
-            ><img src={param} /></div>
-        );
+  console.log(images);
+  return (
+    <div
+      style={{
+        backgroundColor: "#588f58",
+        backgroundImage: images,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        width: "100%",
+        height: "700px",
+      }}
+    ></div>
+  );
+};
 
-}
-
-export default Field ;
+export default Field;
