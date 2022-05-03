d753d8b3da6352aa91992dfe2392b5f9835af11f
Nicolas Martinez <52309078+Martinez-Latorraca@users.noreply.github.com>
2022-05-02 22:46:55 -0300

    patron fondo de cancha, falta el select

--- src/App.js
+++ src/App.js
@@ -1,60 +1,20 @@
-import {useEffect, useState} from 'react';
-import './App.css';
-import Field from './field.jsx';
-import circles from './circles.png';
-import squares from './squares.png';
-import lines from './lines.png';
-
+import { useState } from "react";
+import "./App.css";
+import Field from "./field.jsx";
 
 function App() {
-
-  const options = [
-    {
-      label: "Circles",
-      value: "Circles",
-    },
-    {
-      label: "Lines",
-      value: "Lines",
-    },
-    {
-      label: "Squares",
-      value: "Squares",
-    },
-    
-  ];
-  const [pattern, setPattern] = useState("Circle"); 
-
-  const handleSelect = (e) => {
-    console.log(e.target.value)
-    setPattern(e); 
-    
-  }
-  
-  const getImagePattern = () => {
-    console.log(pattern , "param");
-
-    switch (pattern) {
-        case "Lines":
-            return lines;
-        case "Squares":
-            return squares;
-        case "Circles":
-            return circles;
-        default:  return circles;
-    }
-}
-
+  const [pattern, setPattern] = useState("Circle");
 
   return (
     <div className="App">
-      <Field param={getImagePattern()}></Field>
-      <select  onChange={handleSelect}>
-            {options.map((option) => (
-              <option value={option.value}>{option.label}</option>
-            ))}
-          </select>
-
+      <Field param={pattern}></Field>
+      <select onChange={(e) => setPattern(e.target.value)}>
+        <option disabled>Selecciona la decoracion</option>
+        <option value="Circles">Circulos</option>
+        <option value="Lines">Lineas</option>
+        <option value="Squares">Cuadrados</option>
+      </select>
+      <div></div>
     </div>
   );
 }
